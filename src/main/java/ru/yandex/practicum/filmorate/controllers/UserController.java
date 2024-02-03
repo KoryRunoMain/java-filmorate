004c235107f3b5f19e5f9412754e5549e7e0a954
koryruno <koryruno@yandex.ru>
2024-02-03 17:01:03 +0300

    structure v/1.1

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.models.User;
 import ru.yandex.practicum.filmorate.validations.ValidateUser;
 import javax.validation.Valid;
+import javax.validation.ValidationException;
+import java.time.LocalDate;
 import java.util.*;
 
 @RestController
@@ -14,7 +16,6 @@ import java.util.*;
 @Slf4j
 public class UserController {
 
-    private final ValidateUser validate = new ValidateUser();
     private final Map<Integer, User> userStorage = new HashMap<>();
 
     @GetMapping
@@ -26,7 +27,7 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> createUser(@Valid @RequestBody User newUser) {
         if (!userStorage.containsKey(newUser.getId())) {
-            validate.validateUser(newUser);
+            validateUser(newUser);
             newUser.setId(userStorage.size() + 1);
             userStorage.put(newUser.getId(), newUser);
             return new ResponseEntity<>(newUser, HttpStatus.CREATED);
@@ -36,10 +37,25 @@ public class UserController {
 
     @PutMapping
     public ResponseEntity<User> updateOrCreateUser(@Valid @RequestBody User user) {
-        validate.validateUser(user);
+        validateUser(user);
         userStorage.put(user.getId(), user);
         log.info("Пользователь добавлен или обновлен");
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    private void validateUser(User user) {
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            log.info("ошбка проверки поля почты");
+            throw new ValidationException("Электронная почта не может быть пустой. пример: example@example.com");
+        }
+        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            log.info("ошбка проверки поля логина");
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        }
+        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
+            log.info("ошбка проверки поля даты рождения");
+            throw new ValidationException("Дата рождения не может быть у будущем");
+        }
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/validations/ValidateUser.java
+++ src/main/java/ru/yandex/practicum/filmorate/validations/ValidateUser.java
@@ -1,24 +1,25 @@
 package ru.yandex.practicum.filmorate.validations;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.models.User;
 import javax.validation.ValidationException;
 import java.time.LocalDate;
 
+@Slf4j
 public class ValidateUser {
 
     public void validateUser(User user) {
         if (user.getEmail() == null || !user.getEmail().contains("@")) {
-            // добавить ЛОГ
+            log.info("ошбка проверки поля почты");
             throw new ValidationException("Электронная почта не может быть пустой. пример: example@example.com");
         }
         if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
-            // добавить ЛОГ
+            log.info("ошбка проверки поля логина");
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         }
         if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
-            // добавить ЛОГ
+            log.info("ошбка проверки поля даты рождения");
             throw new ValidationException("Дата рождения не может быть у будущем");
         }
     }
-
 }
