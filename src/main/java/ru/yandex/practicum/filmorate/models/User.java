6ae5289e1e6f284fc39d2e180da52f6765c1bc67
koryruno <koryruno@yandex.ru>
2024-02-07 14:36:06 +0300

    убрал метод getNameIfEmpty(). Перенес проверку в класс UserService в метод validateUser(). Полагаю в User не стоит держать методы работы с полями.

--- src/main/java/ru/yandex/practicum/filmorate/models/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/User.java
@@ -1,15 +1,20 @@
 package ru.yandex.practicum.filmorate.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-import javax.validation.constraints.*;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 /**
  * User
  */
 @Data
-@Slf4j
+@AllArgsConstructor
 public class User {
 
     private Integer id;
@@ -27,22 +32,5 @@ public class User {
     @NotNull(message = "дата рождения не может быть null")
     @Past(message = "дата рождения не может быть в будущем")
     private LocalDate birthday;
-
-    public User(Integer id, String email, String login, LocalDate birthday, String name) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.birthday = birthday;
-        this.name = getNameIfEmpty(name, login);
-    }
-
-    // имя для отображения может быть пустым — в таком случае будет использован логин
-    private String getNameIfEmpty(String name, String login) {
-        if (name == null || name.isBlank() || name.trim().isEmpty()) {
-            log.info("Поле имени использует адрес эл.почты");
-            return login;
-        }
-        return name;
-    }
 
 }
