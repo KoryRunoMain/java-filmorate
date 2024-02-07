e15331b5e0cc9df85befcd4781a3e1b6de3146ae
koryruno <koryruno@yandex.ru>
2024-02-07 14:38:56 +0300

    убрал метод getNameIfEmpty(). Перенес проверку в класс UserService в метод validateUser(). Полагаю в User не стоит держать методы работы с полями.

--- src/main/java/ru/yandex/practicum/filmorate/models/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/User.java
@@ -2,9 +2,7 @@ package ru.yandex.practicum.filmorate.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
