0ac0cb0c9a40b1f1d41026aa44ce10165f737a9e
koryruno <koryruno@yandex.ru>
2024-02-27 11:11:17 +0300

    Ready for review

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/InternalServerException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/InternalServerException.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class InternalServerException extends Throwable {
-    public InternalServerException (String message) {
+    public InternalServerException(String message) {
         super(message);
         log.error(message);
     }
