916e35edc11f42a2e59106644cd7b245b427e32c
koryruno <koryruno@yandex.ru>
2024-03-12 14:43:09 +0300

    add new controllers

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/MPARatingController.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mpa_ratings")
+public class MPARatingController {
+}
