8545d0f904c232d84e11bda67a3dc3d0955e38de
koryruno <koryruno@yandex.ru>
2024-03-24 19:17:40 +0300

    override Equals & Hash

--- src/main/java/ru/yandex/practicum/filmorate/models/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/models/Film.java
@@ -47,17 +47,13 @@ public class Film implements Model {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Film film = (Film) o;
-        return duration == film.duration
-                && Objects.equals(name, film.name)
-                && Objects.equals(description, film.description)
-                && Objects.equals(releaseDate, film.releaseDate);
+        return Objects.equals(id, film.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, description, releaseDate, duration);
+        return Objects.hash(id);
     }
-
 }
 
 
